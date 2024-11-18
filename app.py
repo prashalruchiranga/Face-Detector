f870246c4bca48b3fdfba28a5649dfa131dcaf28
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2024-11-18 20:57:38 +0530

    Update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 
 def open_camera(): 
     camera = cv2.VideoCapture(0)
+    
     if not camera.isOpened():
         print("Error: Could not open camera.")
         exit()
@@ -11,11 +12,9 @@ def open_camera():
         if not ret:
             print("Failed to grab frame.")
             break
-
         cv2.imshow("Face Detector", frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
+
     camera.release()
     cv2.destroyAllWindows()
