b082c36e06f6cabe0f2d587ca662e06797232705
Prashal Ruchiranga <prashalruchiranga@gmail.com>
2024-12-15 00:17:00 +0530

    Call annotate_faces_in_video from main method

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from app import annotate_faces_in_video
 
 def main():
+    # Path to the video file
+    weights_path = "tmp/checkpoints/ssd7_epoch-13_loss-2.0928_val_loss-2.1003.keras"
+    img_height = 512
+    img_width = 512
+    video_name = "Obama jokingly calls Biden Vice President Biden during White House event.mp4"
+    annotate_faces_in_video(weights_path, img_height, img_width, video_name)
     print("Program is running")
 
 if __name__ == "__main__":
